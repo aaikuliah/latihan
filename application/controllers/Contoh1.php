86efc1fb66e4db35f14e4755ba41317ea88da1e7
anikaai <anik.aai.kuliah@gmail.com>
2022-09-19 17:02:27 +0700

    .

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo"Nama saya Anik Andriani
+ Saya tinggal dari Sleman, Yogyakarta
+ olah raga yang saya sukai adalah
+ Bulutangkis";
+ } }
